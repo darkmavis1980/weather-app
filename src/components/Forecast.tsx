763ebe0dd2a3835dddf084709da7f06eb1eeeaa7
Alessio Michelini <darkmavis1980@gmail.com>
2024-02-18 21:44:19 +0000

    feat: add logic for weather icon

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,38 +1,42 @@
 import { useState, useEffect } from 'react';
 import useSWR from 'swr';
 import { api } from '../lib/api';
-import Cloudy from '../svg/cloudy-day-1.svg';
+import { ICON_MAP } from '../lib/config';
 
 const REFRESH_TIME = 1000 * 60;
 
-const fetcher = (url: string) => api.get(url).then(({data}) => data);
+interface Forecast {
+  icon: string;
+  temperature: number;
+}
+
+const fetcher = (url: string) => api.get<Forecast>(url).then(({data}) => data);
 
 const Forecast = () => {
+  const { data: temperatures } = useSWR(`/forecast`, fetcher, {
+    suspense: true,
+    refreshInterval: REFRESH_TIME,
+  });
 
-    const { data: temperatures } = useSWR(`/forecast`, fetcher, {
-        suspense: true,
-        refreshInterval: REFRESH_TIME,
-    });
+  const [myTime, setMyTime] = useState(new Date());
 
-    const [myTime, setMyTime] = useState(new Date());
-
-    useEffect(() => {
-        const timerID = setInterval(() => tick(), REFRESH_TIME);
-        
-        return () => clearInterval(timerID);
-    }, [myTime, temperatures]);
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), REFRESH_TIME);
     
-    function tick() {
-        const date = new Date();
-        setMyTime(date);
-    }
+    return () => clearInterval(timerID);
+  }, [myTime, temperatures]);
+  
+  function tick() {
+    const date = new Date();
+    setMyTime(date);
+  }
 
-    return (
-        <>
-            <img src={Cloudy} alt="Weather" width="200" />
-            <h2>Outdoor: {temperatures && Math.round(temperatures.temperature)}&deg;</h2>
-        </>
-    )
+  return (
+    <>
+      {temperatures && (<img src={ICON_MAP[temperatures.icon]} alt="Weather" width="200" />)}
+      <h2>Outdoor: {temperatures && Math.round(temperatures.temperature)}&deg;</h2>
+    </>
+  )
 }
 
 export default Forecast;
---
+++ src/lib/config.ts
@@ -0,0 +1,14 @@
+import Snow from '../svg/snowy-1.svg';
+import Rain from '../svg/rainy-1.svg';
+import Cloud from '../svg/cloudy.svg';
+import Sun from '../svg/day.svg';
+import Storm from '../svg/thunder.svg';
+
+export const ICON_MAP: Record<string, string> = {
+  "snow": Snow,
+  "rain": Rain,
+  "cloud": Cloud,
+  "sun": Sun,
+  "storm": Storm,
+  "wind": Sun,
+}
