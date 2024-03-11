2a91ec5ae7717dc868afe282f4a0e3fe0bfff330
Alessio Michelini <darkmavis1980@gmail.com>
2024-03-11 10:45:30 +0000

    fix: update bash script to run container

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ docker stop weather
 
 docker build -t weather-app .
 
-docker run --name weather --rm -d -p 8089:80 weather-app
+docker run --name weather -d -p 8089:80 --restart unless-stopped weather-app
